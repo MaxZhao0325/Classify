dceb8892fe396878916556f97614e0a199902381
Hanzhang Zhao <112364907+MaxZhao0325@users.noreply.github.com>
2023-11-08 13:11:24 -0500

    set Spring2024 1242 as default

--- classify/tasks2.py
+++ classify/tasks2.py
@@ -1,3 +1,15 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cs3240a17.settings')  # replace 'myproject.settings' with your actual settings module
+django.setup()
+
 from classify.models import Class, Dept, Profile, ProfileForm, Schedule, ScheduleForm, Friend_Request, Comment
 for profile in Profile.objects.all():
     print(profile.courses)
+
+# Retrieve multiple instances
+instances = Class.objects.filter(semester_code='1242')  # Retrieves all instances that match the filter
+
+# Delete the instances
+print(len(instances))
